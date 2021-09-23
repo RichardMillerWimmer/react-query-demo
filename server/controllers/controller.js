342d3b6544d33b470267a5d4f808315c68dd10f0
Richard Miller <richardmmill@gmail.com>
2021-09-23 16:59:32 -0400

    working on map and axios call

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,44 +1,56 @@
-require('dotenv').config();
+require("dotenv").config();
 const axios = require("axios");
 
-const {REACT_APP_RAPID_API_HOST,  REACT_APP_RAPID_API_KEY} = process.env;
+const { REACT_APP_RAPID_API_HOST, REACT_APP_RAPID_API_KEY } = process.env;
 
-const portfolio = ['1'];
+const portfolio = ["1"];
 
-function callPortfolio() {
-    let fullPortfolio = portfolio.map(coin => {
-        axios.get(`https://coinranking.p.rapidapi.com/coin/${coin}`, {headers: {
-            'x-rapidapi-host': REACT_APP_RAPID_API_HOST,
-            'x-rapidapi-key': REACT_APP_RAPID_API_KEY
-        }})
-        .then((res) => {
-            console.log('fullPorfolio call', res.data.data)
-            fullPortfolio.push(res.data.data)
+async function callPortfolio() {
+    let fullPortfolio = await Promise.all(
+        portfolio.map((coin) => {
+            axios.get(`https://coinranking.p.rapidapi.com/coin/${coin}`, {
+                headers: {
+                    "x-rapidapi-host": REACT_APP_RAPID_API_HOST,
+                    "x-rapidapi-key": REACT_APP_RAPID_API_KEY,
+                },
+            });
         })
-    })
-    Promise.all(fullPortfolio)
-    .then(res => {
-        console.log('fullPorfolio', res)
+    );
+    console.log(fullPortfolio);
+    return fullPortfolio;
+    // let fullPortfolio = portfolio.map(coin => {
+    //     axios.get(`https://coinranking.p.rapidapi.com/coin/${coin}`, {headers: {
+    //         'x-rapidapi-host': REACT_APP_RAPID_API_HOST,
+    //         'x-rapidapi-key': REACT_APP_RAPID_API_KEY
+    //     }})
+    //     .then((res) => {
+    //         console.log('fullPorfolio call', res.data.data)
+    //         fullPortfolio.push(res.data.data)
+    //     })
+    // })
+    // Promise.all(fullPortfolio)
+    // .then(res => {
+    //     console.log('fullPorfolio', res)
 
-    })
+    // })
     // return fullPortfolio
 }
 
 module.exports = {
     getPortfolio: async (req, res) => {
         const portfolioRes = await callPortfolio();
-        console.log('getPortfolio call', portfolioRes)
-        res.status(200).send(portfolioRes)
+        console.log("getPortfolio call", portfolioRes);
+        res.status(200).send(portfolioRes);
     },
     addCurrency: (req, res) => {
-        const {newCurrency} = req.body;
+        const { newCurrency } = req.body;
         portfolio.push(newCurrency);
         res.status(200).send(portfolio);
     },
     deleteCurrency: (req, res) => {
-        const {id} = req.params;
-        const currencyRemove = portfolio.findIndex(coin => coin.id === +id);
+        const { id } = req.params;
+        const currencyRemove = portfolio.findIndex((coin) => coin.id === +id);
         portfolio.splice(currencyRemove, 1);
         res.status(200).send(portfolio);
-    }
+    },
 };
